aed65340770aeedf3517eb92a7fbfdeb91b9eb4f
Henrik Christensen <sensen1695@hotmail.com>
2022-01-17 18:29:37 +0100

    added type, size and items properties

--- src/StaticArray.php
+++ src/StaticArray.php
@@ -2,9 +2,73 @@
 
 namespace Henrikac\StaticArray;
 
+/**
+ * Implementation of a static array that can only store items of a single type
+ * and has a fixed size
+ *
+ * @author Henrik Christensen <sensen1695@hotmail.com>
+ */
 class StaticArray
 {
+    /**
+     * The type of the items in the array
+     *
+     * @var string
+     */
+    private string $type;
+
+    /**
+     * The size of the array
+     *
+     * @var integer
+     */
+    private int $size;
+
+    /**
+     * The items in the array
+     *
+     * @var array
+     */
+    private array $items = [];
+
+    /**
+     * Initializes a new static array that can only store items of the given type
+     * and has a fixed size of the given size
+     *
+     * @param string $type
+     * @param integer $size
+     */
     public function __construct(string $type, int $size)
     {
+        if (!isset($type) || empty($type)) {
+            throw new \InvalidArgumentException('Undefined type');
+        }
+
+        if ($size < 0) {
+            throw new \InvalidArgumentException('Size must be greater than or equal to zero');
+        }
+
+        $this->type = $type;
+        $this->size = $size;
+    }
+
+    /**
+     * Returns the type of items in the array
+     *
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Returns the size of the array
+     *
+     * @return integer
+     */
+    public function getSize(): int
+    {
+        return $this->size;
     }
 }
--- tests/StaticArrayTest.php
+++ tests/StaticArrayTest.php
@@ -12,4 +12,44 @@ class StaticArrayTest extends TestCase
         $this->assertNotNull($arr);
         $this->assertInstanceOf(StaticArray::class, $arr);
     }
+
+    public function testGetTypeReturnsCorrectType(): void
+    {
+        $arr = new StaticArray(int::class, 5);
+
+        $expected = 'int';
+        $actual = $arr->getType();
+
+        $this->assertEquals(
+            $expected,
+            $actual
+        );
+    }
+
+    public function testInvalidArgumentExceptionIsThrownIfTypeIsEmpty(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        new StaticArray('', 3);
+    }
+
+    public function testGetSizeReturnsCorrectSize(): void
+    {
+        $expected = 10;
+        $arr = new StaticArray(int::class, $expected);
+
+        $actual = $arr->getSize();
+
+        $this->assertEquals(
+            $expected,
+            $actual
+        );
+    }
+    
+    public function testInvalidArgumentExceptionIsThrownIfSizeIsLessThanZero(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        new StaticArray(string::class, -1);
+    }
 }
