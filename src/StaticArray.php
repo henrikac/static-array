898cdb5c330318cbf6fc601a826bf7a3721982d5
Henrik Christensen <sensen1695@hotmail.com>
2022-01-20 20:59:36 +0100

    implemented ArrayAccess

--- src/StaticArray.php
+++ src/StaticArray.php
@@ -2,30 +2,32 @@
 
 namespace Henrikac\StaticArray;
 
+use ArrayAccess;
+
 /**
  * Implementation of a static array that can only store items of a single type
- * and has a fixed size
+ * and has a fixed size.
  *
  * @author Henrik Christensen <sensen1695@hotmail.com>
  */
-class StaticArray
+class StaticArray implements ArrayAccess
 {
     /**
-     * The type of the items in the array
+     * The type of the items in the array.
      *
      * @var string
      */
     private string $type;
 
     /**
-     * The size of the array
+     * The size of the array.
      *
      * @var integer
      */
     private int $size;
 
     /**
-     * The items in the array
+     * The items in the array.
      *
      * @var array
      */
@@ -33,12 +35,16 @@ class StaticArray
 
     /**
      * Initializes a new static array that can only store items of the given type
-     * and has a fixed size of the given size
+     * and has a fixed size of the given size.
+     * 
+     * ```
+     * $arr = new StaticArray(int::class, 10);
+     * ```
      *
      * @param string $type
      * @param integer $size
      * 
-     * @throws \InvalidArgumentException If type is not defined or if size is less than 0
+     * @throws \InvalidArgumentException If type is not defined or if size is less than 0.
      */
     public function __construct(string $type, int $size)
     {
@@ -55,7 +61,94 @@ class StaticArray
     }
 
     /**
-     * Returns the type of items in the array
+     * Returns whether the given offset exists.
+     *
+     * @param mixed $offset
+     * @return boolean
+     * 
+     * @throws \InvalidArgumentException If offset is not an integer.
+     * @throws \OutOfRangeException If offset is less than 0 or greater than or equal to the size of the array.
+     */
+    public function offsetExists(mixed $offset): bool
+    {
+        $this->throwIfInvalidOffset($offset);
+
+        return isset($this->items[$offset]);
+    }
+
+    /**
+     * Returns the item at the given offset if set, otherwise null is returned.
+     *
+     * @param mixed $offset
+     * @return mixed
+     * 
+     * @throws \InvalidArgumentException If offset is not an integer.
+     * @throws \OutOfRangeException If offset is less than 0 or greater than or equal to the size of the array.
+     */
+    public function offsetGet(mixed $offset): mixed
+    {
+        $this->throwIfInvalidOffset($offset);
+
+        return isset($this->items[$offset]) ? $this->items[$offset] : null;
+    }
+
+    /**
+     * Assigns the given value to the specified offset.
+     *
+     * @param mixed $offset
+     * @param mixed $value
+     * @return void
+     * 
+     * @throws \InvalidArgumentException If offset is not an integer or if the given value has an incorrect type.
+     * @throws \OutOfRangeException If offset is not null, less than 0 or greater than or equal to the size of the array.
+     * @throws \OverflowException If the array is already full.
+     */
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        $this->throwIfInvalidOffset($offset);
+
+        if (count($this->items) === $this->size) {
+            throw new \OverflowException('Cannot add items to a full array');
+        }
+
+        $valueType = gettype($value);
+
+        // This is a very naive hack!
+        // TODO: come up with a better solution
+        if ($valueType !== $this->type) {
+            switch ($valueType) {
+                case 'boolean' && $this->type === 'bool':
+                    break;
+                case 'integer' && $this->type === 'int':
+                    break;
+                case 'object' && $this->type === $value::class:
+                    break;
+                default:
+                    throw new \InvalidArgumentException(sprintf('Expected item of type %s but got %s', $this->type, gettype($value)));
+            }
+        }
+
+        $this->items[$offset] = $value;
+    }
+
+    /**
+     * Unset the given offset.
+     *
+     * @param mixed $offset
+     * @return void
+     * 
+     * @throws \InvalidArgumentException If offset is not an integer.
+     * @throws \OutOfRangeException If offset is less than 0 or greater than or equal to the size of the array.
+     */
+    public function offsetUnset(mixed $offset): void
+    {
+        $this->throwIfInvalidOffset($offset);
+
+        unset($this->items[$offset]);
+    }
+
+    /**
+     * Returns the type of items in the array.
      *
      * @return string
      */
@@ -65,12 +158,32 @@ class StaticArray
     }
 
     /**
-     * Returns the size of the array
+     * Returns the size of the array.
      *
      * @return integer
      */
     public function getSize(): int
     {
         return $this->size;
+    }
+
+    /**
+     * Checks whether the offset is a valid offset, if not valid an exception is thrown.
+     *
+     * @param mixed $offset
+     * @return void
+     * 
+     * @throws \InvalidArgumentException If offset is not an integer.
+     * @throws \OutOfRangeException If offset is less than 0 or greater than or equal to the size of the array.
+     */
+    private function throwIfInvalidOffset(mixed $offset): void
+    {
+        if (!isset($offset) || !is_int($offset)) {
+            throw new \InvalidArgumentException(sprintf('Expected offset of type int but got %s', gettype($offset)));
+        }
+
+        if ($offset < 0 || $offset >= $this->size) {
+            throw new \OutOfRangeException(sprintf('Expected an offset between 0 and %d but got %d', $this->size - 1, $offset));
+        }
     }
 }
--- tests/StaticArrayTest.php
+++ tests/StaticArrayTest.php
@@ -3,6 +3,8 @@
 use Henrikac\StaticArray\StaticArray;
 use PHPUnit\Framework\TestCase;
 
+class TestClass {}
+
 class StaticArrayTest extends TestCase
 {
     public function testCanInstantiateStaticArray(): void
@@ -51,5 +53,134 @@ class StaticArrayTest extends TestCase
         $this->expectException(\InvalidArgumentException::class);
 
         new StaticArray(string::class, -1);
+    }
+
+    public function testOffsetSetCanSetValueCorrectly(): void
+    {
+        $arr = new StaticArray(string::class, 3);
+
+        $arr[0] = 'Alice';
+        $arr[1] = 'Bob';
+        $arr[2] = 'Eve';
+
+        $this->assertEquals('Eve', $arr[2]);
+    }
+
+    public function testOffsetSetCanHandleClassesToo(): void
+    {
+        $arr = new StaticArray(TestClass::class, 2);
+
+        $arr[0] = new TestClass();
+        $arr[1] = new TestClass();
+
+        $this->assertInstanceOf(TestClass::class, $arr[0]);
+    }
+
+    public function testOffsetExistsReturnsTrueIfOffsetIsSet(): void
+    {
+        $arr = new StaticArray(int::class, 4);
+
+        $arr[0] = (int)4;
+        $arr[1] = (int)7;
+        $arr[2] = (int)2;
+
+        $this->assertTrue(isset($arr[1]));
+    }
+
+    public function testOffsetExistsReturnsFalseIfOffsetIsNotSet(): void
+    {
+        $arr = new StaticArray(int::class, 4);
+
+        $arr[0] = (int)4;
+        $arr[1] = (int)7;
+        $arr[2] = (int)2;
+
+        $this->assertFalse(isset($arr[3]));
+    }
+
+    public function testOffsetExistsThrowsInvalidArgumentExceptionIfOffsetIsNotAnInt(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $arr = new StaticArray(int::class, 4);
+
+        isset($arr['bob']);
+    }
+
+    public function testOffsetExistsThrowsOutOfRangeExceptionIfOffsetIsOutOfRange(): void
+    {
+        $this->expectException(\OutOfRangeException::class);
+
+        $arr = new StaticArray(int::class, 4);
+
+        isset($arr[4]);
+    }
+
+    public function testOffsetGetReturnsCorrectValue(): void
+    {
+        $arr = new StaticArray(int::class, 4);
+
+        $arr[0] = (int)4;
+        $arr[1] = (int)7;
+        $arr[2] = (int)2;
+
+        $this->assertEquals(7, $arr[1]);
+    }
+
+    public function testOffsetGetThrowsInvalidArgumentExceptionIfOffsetIsNotAnInt(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $arr = new StaticArray(int::class, 4);
+
+        $arr[0] = (int)4;
+        $arr[1] = (int)7;
+        $arr[2] = (int)2;
+
+        $arr['bob'];
+    }
+
+    public function testOffsetGetThrowsOutOfRangeExceptionIfOffsetIsOutOfRange(): void
+    {
+        $this->expectException(\OutOfRangeException::class);
+
+        $arr = new StaticArray(int::class, 4);
+
+        $arr[4];
+    }
+
+    public function testOffsetUnsetUnsetsOffset(): void
+    {
+        $arr = new StaticArray(int::class, 4);
+
+        $arr[0] = (int)4;
+        $arr[1] = (int)7;
+        $arr[2] = (int)2;
+
+        unset($arr[1]);
+
+        $this->assertNull($arr[1]);
+    }
+
+    public function testOffsetUnsetThrowsInvalidArgumentExceptionIfOffsetIsNotAnInt(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        $arr = new StaticArray(int::class, 4);
+
+        $arr[] = (int)4;
+        $arr[] = (int)7;
+        $arr[] = (int)2;
+
+        unset($arr['bob']);
+    }
+
+    public function testOffsetUnsetThrowsOutOfRangeExceptionIfOffsetIsOutOfRange(): void
+    {
+        $this->expectException(\OutOfRangeException::class);
+
+        $arr = new StaticArray(int::class, 4);
+
+        unset($arr[4]);
     }
 }
